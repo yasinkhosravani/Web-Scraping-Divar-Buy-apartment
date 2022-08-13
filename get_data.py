d8cfd4390866779be0704816a3ae1337086d77c9
yasin khosravani <31961300+yasinkhosravani@users.noreply.github.com>
2022-08-13 16:34:44 +0000

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -2,37 +2,39 @@
 # coding: utf-8
 
 # In[348]:
-
+import time
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 pd.set_option('display.max_colwidth', 500)
 import requests
 
+while True: #Infinite loop
 
-
-page = requests.get("https://divar.ir/s/shiraz/buy-apartment")
-soup = bs(page.content)
-x=str(soup.find_all(class_='kt-post-card__title'))
-x=x.replace('</h2>',' ').replace('<h2 class="kt-post-card__title">',' ')
-x=x.split(", ")
-z=len(x)
-
-
-def title(l):
+    page = requests.get("https://divar.ir/s/shiraz/buy-apartment")
+    soup = bs(page.content)
     x=str(soup.find_all(class_='kt-post-card__title'))
     x=x.replace('</h2>',' ').replace('<h2 class="kt-post-card__title">',' ')
     x=x.split(", ")
     z=len(x)
-    return x[l]
-def price(l):      
-    x=str(soup.find_all(class_='kt-post-card__description'))
-    x=x.replace('</div>',' ').replace('<div class="kt-post-card__description">',' ')
-    x=x.split(", ")        
-    return x[l]
 
 
-for i in range(0 ,z):
-    print(title(i),price(i))           
+    def title(l):
+        x=str(soup.find_all(class_='kt-post-card__title'))
+        x=x.replace('</h2>',' ').replace('<h2 class="kt-post-card__title">',' ')
+        x=x.split(", ")
+        z=len(x)
+        return x[l]
+    def price(l):      
+        x=str(soup.find_all(class_='kt-post-card__description'))
+        x=x.replace('</div>',' ').replace('<div class="kt-post-card__description">',' ')
+        x=x.split(", ")        
+        return x[l]
 
+
+    for i in range(0 ,z):
+        print(title(i),price(i))
+
+
+    time.sleep(60*60) #Wait 3600s (60 min) before re-entering the cycle
 
 
